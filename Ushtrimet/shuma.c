432d3013de03039484c49bfcc041e31b9263f4d6
Florian Saqipi <49369376+floriansaqipi@users.noreply.github.com>
2023-03-24 12:03:40 +0100

    help commands

---
+++ Ushtrimet/shuma.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+//int shuma (int x, int y){
+//	return x+y;
+//}
+
+int main(){
+	int a = 8;
+	int b = 3;
+	printf("Shuma %d + %d = %d\n", a,b, a+b);
+	//printf("%d\n",shuma(2,3));
+	return 0;
+}
